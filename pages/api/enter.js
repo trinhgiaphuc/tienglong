caf21b624f289c9e4dbc7527a3bf01ea2d50d541
Phuc Trinh <trinhgiaphuc2k@gmail.com>
2022-03-10 10:29:52 +0700

    add auth wrapepr

---
+++ pages/api/enter.js
@@ -0,0 +1,35 @@
+import cookie from 'cookie';
+
+export default async function handler(req, res) {
+  const { authorization } = req.headers;
+  const { login, logout } = req.body;
+
+  if (login) {
+    const token = authorization.split(' ')[1];
+
+    res.setHeader(
+      'Set-Cookie',
+      cookie.serialize('USER_ACCESS_TOKEN', token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        path: '/',
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production',
+      })
+    );
+  } else if (logout) {
+    res.setHeader(
+      'Set-Cookie',
+      cookie.serialize('USER_ACCESS_TOKEN', '', {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        expires: new Date(Date.now() - 1),
+        path: '/',
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production',
+      })
+    );
+  }
+
+  return res.status(200).json({ ok: 'ok' });
+}
