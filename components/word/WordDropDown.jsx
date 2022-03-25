2f323df8bc4e649ea775999d26005611ea8d08f9
Phuc Trinh <trinhgiaphuc2k@gmail.com>
2022-03-25 10:35:38 +0700

    fix dropdown 4

--- components/word/WordDropDown.jsx
+++ components/word/WordDropDown.jsx
@@ -19,19 +19,15 @@ const WordDropDown = ({ authorId }) => {
   };
 
   return (
-    <div
+    <button
       type="button"
       id="menu-button"
       aria-expanded="true"
       aria-haspopup="true"
       className="absolute top-0 right-0 pt-2 pr-2 pb-1 pl-1 bg-transparent outline-none"
+      onClick={handleToggle}
     >
-      <button
-        onClick={handleToggle}
-        className="rounded-full  hover:bg-gray-50 outline-none p-2"
-      >
-        <IoEllipsisVertical className="prose prose-2xl outline-none aspect-square  font-medium" />
-      </button>
+      <IoEllipsisVertical className="prose prose-2xl outline-none aspect-square rounded-full  hover:bg-gray-50 font-medium" />
       <div
         className={`absolute top-full right-3/4 rounded-md shadow-lg overflow-hidden bg-gray-100 ring-1 ring-black ring-opacity-5 opacity-0 scale-0 duration-150  ${
           toggle ? 'opacity-100 scale-100' : ''
@@ -40,24 +36,24 @@ const WordDropDown = ({ authorId }) => {
       >
         {user?.id === authorId ? (
           <div id="choice" className="" role="none">
-            <button
-              className="p-2 prose min-w-max bg-gray-100 hover:bg-zinc-300"
+            <a
+              className="p-2 block  prose min-w-max bg-gray-100 hover:bg-zinc-300"
               tabIndex="-1"
             >
               Chỉnh Sửa
-            </button>
+            </a>
           </div>
         ) : null}
         <div id="choice" className="" role="none">
-          <button
-            className="p-2 prose min-w-max bg-gray-100 hover:bg-zinc-300 "
+          <a
+            className="p-2 block prose min-w-max bg-gray-100 hover:bg-zinc-300 "
             tabIndex="-1"
           >
             Báo Cáo
-          </button>
+          </a>
         </div>
       </div>
-    </div>
+    </button>
   );
 };
 
