53ecba3d981d719333c561efa0ed5a88cb7c5e1f
Phuc Trinh <trinhgiaphuc2k@gmail.com>
2022-03-26 12:05:36 +0700

    fix typography 6

--- components/buttons/ShareButton.jsx
+++ components/buttons/ShareButton.jsx
@@ -2,7 +2,7 @@ import { IoShareSocialOutline } from 'react-icons/io5';
 
 const ShareButton = () => {
   return (
-    <button className="active:scale-75 duration-700 mx-2 lg:mx-0">
+    <button className="active:scale-75 duration-700 mx-2 lg:mx-0 h-full">
       <div className="word-button h-full">
         <IoShareSocialOutline className="prose prose-2xl text-black" />
         <p className="prose prose-sm lg:prose-lg text-black xl:prose-xl font-medium hidden sm:block">
