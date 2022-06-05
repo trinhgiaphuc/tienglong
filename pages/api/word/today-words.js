7a9d7b0668fc263eed1817b02dc6cacc9c6c1cfe
Phuc Trinh <trinhgiaphuc2k@gmail.com>
2022-06-05 09:41:09 +0700

    change fetch today-words style

--- pages/api/word/today-words.js
+++ pages/api/word/today-words.js
@@ -4,12 +4,6 @@ export default async function handler(req, res) {
   try {
     const todayWords = JSON.parse(JSON.stringify(await getTodayWords()));
     // const trengdingWords = await getTrendingWords();
-
-    res.setHeader(
-      'Cache-Control',
-      'public, s-maxage=10, stale-while-revalidate=59'
-    );
-
     return res.status(200).json({ todayWords });
   } catch (error) {
     console.error(error);
