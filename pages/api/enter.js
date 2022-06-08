4327f930c971329171300dca30737c0ead839f5b
Phuc Trinh <trinhgiaphuc2k@gmail.com>
2022-06-08 10:24:48 +0700

    fix logged in but not create cookie

--- pages/api/enter.js
+++ pages/api/enter.js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
   const { authorization } = req.headers;
   const { login, logout } = req.body;
 
+  console.log(authorization);
   if (login) {
     const token = authorization.split(' ')[1];
     res.setHeader(
