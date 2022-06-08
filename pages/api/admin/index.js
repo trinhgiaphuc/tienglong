8e9938c8fa2b8bd8c3b18e45ec0cf3640c7976c9
Phuc Trinh <trinhgiaphuc2k@gmail.com>
2022-06-08 14:28:47 +0700

    admin auth debug

--- pages/api/admin/index.js
+++ pages/api/admin/index.js
@@ -6,7 +6,7 @@ import { isAdmin, getSpecificUser } from '@lib/firebase-admin';
 import { createAdminAccount, findAdminWithId } from '@lib/supabase';
 import withAdminAuth from '@lib/withAuthAdmin';
 
-export default withAdminAuth(async function (req, res) {
+const handler = withAdminAuth(async function (req, res) {
   const { password } = req.body;
   const { uid } = req;
 
@@ -14,54 +14,55 @@ export default withAdminAuth(async function (req, res) {
   try {
     user = await getSpecificUser(uid);
   } catch (error) {
-    console.error(error);
     return res.status(500).json({ error, type: 'get user error' });
   }
   if (!isAdmin(user)) {
     return res.status(401).json({ error: 'Tài khoản chưa được cấp quyền.' });
   }
 
-  let { username, image } = user;
-  let adminAccount;
-  try {
-    adminAccount = await checkAdminAccountExist(uid);
-  } catch (error) {
-    return res.status(500).json({ error, type: 'check account error' });
-  }
-  if (!adminAccount) {
-    try {
-      let hashedPassword = await hashPassword(password);
-      adminAccount = await createAdminAccount(
-        uid,
-        username,
-        hashedPassword,
-        image
-      );
-    } catch {
-      return res.status(500).json({ error, type: 'create account error' });
-    }
-  } else {
-    if (!passwordIsTheSame(password, adminAccount.hashedPassword)) {
-      return res.status(401).json({ error: 'Mật mã sai' });
-    }
+  // let { username, image } = user;
+  // let adminAccount;
 
-    let token = jwt.sign({ username, image }, process.env.JWT_SECRET, {
-      expiresIn: '8h',
-    });
+  // try {
+  //   adminAccount = await checkAdminAccountExist(uid);
+  // } catch (error) {
+  //   return res.status(500).json({ error, type: 'check account error' });
+  // }
 
-    res.setHeader(
-      'Set-Cookie',
-      cookie.serialize('ADMIN_ACCESS_TOKEN', token, {
-        httpOnly: true,
-        maxAge: 8 * 60 * 60,
-        path: '/',
-        sameSite: 'strict',
-        secure: process.env.NODE_ENV === 'production',
-      })
-    );
+  // if (!adminAccount) {
+  //   try {
+  //     let hashedPassword = await hashPassword(password);
+  //     adminAccount = await createAdminAccount(
+  //       uid,
+  //       username,
+  //       hashedPassword,
+  //       image
+  //     );
+  //   } catch {
+  //     return res.status(500).json({ error, type: 'create account error' });
+  //   }
+  // } else {
+  //   if (!passwordIsTheSame(password, adminAccount.hashedPassword)) {
+  //     return res.status(401).json({ error: 'Mật mã sai' });
+  //   }
 
-    return res.status(200).json({ ok: true });
-  }
+  //   let token = jwt.sign({ username, image }, process.env.JWT_SECRET, {
+  //     expiresIn: '8h',
+  //   });
+
+  //   res.setHeader(
+  //     'Set-Cookie',
+  //     cookie.serialize('ADMIN_ACCESS_TOKEN', token, {
+  //       httpOnly: true,
+  //       maxAge: 8 * 60 * 60,
+  //       path: '/',
+  //       sameSite: 'strict',
+  //       secure: process.env.NODE_ENV === 'production',
+  //     })
+  //   );
+
+  return res.status(200).json({ ok: user });
+  // }
 });
 
 async function checkAdminAccountExist(uid) {
@@ -87,3 +88,5 @@ async function hashPassword(password) {
 function passwordIsTheSame(password, hashedPassword) {
   return bcrypt.compareSync(password, hashedPassword);
 }
+
+export default handler;
