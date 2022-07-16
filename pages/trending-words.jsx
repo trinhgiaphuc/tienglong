cbb8179b393f3dc9c8877ce415498b96771472a5
Phuc Trinh <trinhgiaphuc2k@gmail.com>
2022-07-16 09:27:03 +0700

    update trending word page

--- pages/trending-words.jsx
+++ pages/trending-words.jsx
@@ -1,12 +1,29 @@
-import { getTrendingWords } from '@lib/firebase-admin';
+import Title from '@components/word/Title';
+import WordDetailList from '@components/word/WordDetailList';
+import fetcher from '@lib/fetcher';
 
-// import SectionWord from '@components/word/SectionWords';
+export default function TodayWordPage({ words }) {
+  const { trendingWords } = words;
 
-export default function TrendingWords({}) {
-  return <div className="h-screen"></div>;
+  return (
+    <div className="h-screen">
+      <div className="border-4 border-black flex items-center justify-center p-10">
+        <Title>Từ Đang Thịnh Hành</Title>
+      </div>
+      <div className="w-full md:w-3/4 mx-auto -mt-1">
+        <WordDetailList isPending={false} words={trendingWords} />
+      </div>
+    </div>
+  );
 }
 
-export async function getServerSideProps(ctx) {
-  const words = await getTrendingWords();
-  return { props: {} };
+export async function getServerSideProps({ req, res }) {
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=300, stale-while-revalidate=300'
+  );
+
+  let words = await fetcher('word/trending-words');
+  return { props: { words } };
 }
+
